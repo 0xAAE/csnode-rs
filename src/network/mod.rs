72e6d0220e00f96b23d5a24b362b8a081742098e
aav@credits.com <aav@credits.com>
2020-02-14 00:23:20 +0300

    Uncompilable

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -9,19 +9,24 @@ use std::sync::mpsc::sync_channel;
 use std::sync::mpsc::{SyncSender, Receiver};
 use log::info;
 
-const TEST_STOP_DELAY_SEC: u64 = 2;
-const MAX_FRAGMENT_SIZE: usize = 1024;
-const MAX_FRAGMENT_QUEUE: usize = 1024;
+extern crate base58;
+use base58::FromBase58;
 
-mod fragment;
+extern crate csp2p_rs;
+use csp2p_rs::CSHost;
+use csp2p_rs::NodeInfo;
+
+const TEST_STOP_DELAY_SEC: u64 = 2;
+const MAX_PACKET_QUEUE: usize = 1024;
+
+//mod fragment;
 mod packet;
-mod fragment_receiver;
+//mod fragment_receiver;
 mod packet_collector;
 
-use fragment::Fragment;
+use packet::Packet;
 
 pub struct Network {
-	recv_thread:		JoinHandle<()>,
 	collect_thread:		JoinHandle<()>,
 	dispatch_thread:	JoinHandle<()>,
 	prepare_thread:		JoinHandle<()>,
@@ -32,11 +37,33 @@ pub struct Network {
 impl Network {
 	pub fn new(conf: SharedConfig) -> Box<Network> {
 		let stop_flag_instance = Arc::new(AtomicBool::new(false));
-		let (tx_fragment, rx_fragment) = sync_channel::<Fragment>(MAX_FRAGMENT_QUEUE);
+		// todo move queue limitation to <pack_collector -> pack_handler> channel
+		let (tx_packet, rx_packet) = sync_channel::<Vec<u8>>(MAX_PACKET_QUEUE);
+
+		// start p2p-compat CSHost
+
+		// get from config
+		let node_id: String;
+		let hosts_filename: String;
+		{
+			let conf_guard = conf.read().unwrap();
+			node_id = conf_guard.node_id.clone();
+			hosts_filename = conf_guard.hosts_filename.clone();
+		}
+	
+		// init host with own id
+		let bytes = node_id[..].from_base58().unwrap(); // base58 -> Vec<u8>
+		let mut host = CSHost::new(&bytes[..], tx_packet).unwrap();
+	
+		// init host entry points list
+		let mut known_hosts = Vec::<NodeInfo>::new();
+		parse_known_hosts_or_default(&mut known_hosts, &hosts_filename);
+		host.add_known_hosts(known_hosts);
+		host.start();
+		
 		let instance = Box::new(Network {
 			stop_flag: stop_flag_instance.clone(),
-			recv_thread: start_recv(conf.clone(), stop_flag_instance.clone(), tx_fragment),
-			collect_thread: start_collect(conf.clone(), stop_flag_instance.clone(), rx_fragment),
+			collect_thread: start_collect(conf.clone(), stop_flag_instance.clone(), rx_packet),
 			dispatch_thread: start_dispatch(conf.clone(), stop_flag_instance.clone()),
 			prepare_thread: start_prepare(conf.clone(), stop_flag_instance.clone()),
 			send_thread: start_send(conf.clone(), stop_flag_instance.clone())
@@ -46,7 +73,6 @@ impl Network {
 
 	pub fn stop(self) {
 		self.stop_flag.store(true, Ordering::SeqCst);
-		self.recv_thread.join().expect("Failed to stop fragment receiver");
 		self.collect_thread.join().expect("Failed to stop packet collector");
 		self.dispatch_thread.join().expect("Failed to stop packet dispatcher");
 		self.prepare_thread.join().expect("Failed to stop send preparator");
@@ -54,28 +80,7 @@ impl Network {
 	}
 }
 
-fn start_recv(conf: SharedConfig, stop_flag: Arc<AtomicBool>, tx: SyncSender<Fragment>) -> JoinHandle<()> {
-	info!("Start fragment receiver");
-	let handle = spawn(move || {
-		info!("Fragment receiver started");
-		let fragment_receiver = fragment_receiver::FragmentReceiver::new(conf, tx);
-		let mut buf = [0u8; MAX_FRAGMENT_SIZE];
-		let mut total_bytes = 0;
-        loop {
-			let cnt = fragment_receiver.recv(&mut buf);
-			if cnt > 0 {
-				total_bytes = total_bytes + cnt;
-			}
-            if stop_flag.load(Ordering::SeqCst) {
-                break;
-            }
-        }
-        info!("Fragment receiver stopped");
-	});
-	handle
-}
-
-fn start_collect(_conf: SharedConfig, stop_flag: Arc<AtomicBool>, rx: Receiver<Fragment>) -> JoinHandle<()> {
+fn start_collect(_conf: SharedConfig, stop_flag: Arc<AtomicBool>, rx: Receiver<Packet>) -> JoinHandle<()> {
 	info!("Start packet collector");
 	let handle = spawn(move || {
 		info!("Packet collector started");
@@ -134,4 +139,57 @@ fn start_send(_conf: SharedConfig, stop_flag: Arc<AtomicBool>) -> JoinHandle<()>
         info!("Fragment sender stopped");
 	});
 	handle
+}
+
+fn parse_known_hosts_or_default(known_hosts: &mut Vec<NodeInfo>, hosts_filename: &String) {
+    if hosts_filename.len() > 0 {
+        match File::open(PathBuf::from(&hosts_filename)) {
+            Err(e) => {
+                println!("Failed to open file {}: {}", &hosts_filename, e);
+                // add well known ru3 as entry point
+                known_hosts.push(
+                    NodeInfo {
+                        id: "HBxj19cnpayn46GSqBGyKQXMaLThH4quuPt5gf8aFndg".from_base58().unwrap(),
+                        ip: "195.133.147.58".to_string(),
+                        port: 9000
+                    }
+                );
+            }
+            Ok(f) => {
+                let reader = BufReader::new(f);
+                for line in reader.lines() {
+                    match line {
+                        Err(_) => continue,
+                        Ok(item) => {
+                            let parts = item.split_whitespace().collect::<Vec<_>>();
+                            if parts.len() != 2 {
+                                println!("Malformed known_hosts record, must conform <ip:port id>, found {}", item);
+                                continue;
+                            }
+                            let addr = parts[0].split(':').collect::<Vec<_>>();
+                            if addr.len() != 2 {
+                                println!("Malformed ip:port part, found {}", parts[0]);
+                                continue;
+                            }
+                            // base58 -> Vec<u8>
+                            let bytes: Vec<u8> = match parts[1].from_base58() {
+                                Err(_) => {
+                                    println!("Malformed id, must be a 32-byte key encoded base58, found {}", parts[1]);
+                                    continue;
+                                }
+                                Ok(b) => b
+                            };
+                            known_hosts.push(
+                                NodeInfo {
+                                    id: bytes,
+                                    ip: addr[0].to_string(),
+                                    port: u16::from_str(addr[1]).unwrap_or(0)
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
