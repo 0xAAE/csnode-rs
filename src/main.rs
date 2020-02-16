4c582f863dc34c76685eecd2e188445d8f30af69
aav@credits.com <aav@credits.com>
2020-02-16 15:27:00 +0300

    Comment unused

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,7 @@ pub const HASH_SIZE: usize = 32;
 
 pub type PublicKey = [u8; PUBLIC_KEY_SIZE];
 //pub type Hash = [u8; HASH_SIZE]; // hash is defined in blake2s
-
-static ZERO_PUBLIC_KEY: PublicKey = [0u8; PUBLIC_KEY_SIZE];
+//static ZERO_PUBLIC_KEY: PublicKey = [0u8; PUBLIC_KEY_SIZE];
 
 fn main() {
     let mut file_name = "config.ini".to_string();
