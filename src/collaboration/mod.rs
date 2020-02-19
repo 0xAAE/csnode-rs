b28085b3ebf10a83745d36b28e6483c9f2e6ef34
Alexander Avramenko <aav@credits.com>
2020-02-19 16:11:09 +0300

    handle and reply with pong on ping from remotes

--- src/collaboration/mod.rs
+++ src/collaboration/mod.rs
@@ -14,14 +14,18 @@ use bincode::serialize_into;
 type Command = super::network::packet::NghbrCmd;
 
 pub struct Collaboration {
-    tx_send: Sender<Packet>
+    tx_send: Sender<Packet>,
+    sequence: u64,
+    round: u64
 }
 
 impl Collaboration {
 
     pub fn new(_conf: SharedConfig, tx_send: Sender<Packet>) -> Collaboration {
         Collaboration {
-            tx_send: tx_send
+            tx_send: tx_send,
+            sequence: 0,
+            round: 0
         }
     }
 
@@ -44,7 +48,7 @@ impl Collaboration {
     fn handle_version_request(&self, sender: &PublicKey, _bytes: Option<&[u8]>) {
         // send version reply:
         let mut output: Vec<u8> = Vec::<u8>::new();
-        match pack_version_reply(&mut output) {
+        match pack_version_reply(&mut output, self.sequence, self.round) {
             Err(_) => {
                 error!("failed to serialize version reply");
             },
@@ -59,7 +63,7 @@ impl Collaboration {
                                 warn!("failed send version reply packet: {}", e);
                             },
                             Ok(_) => {
-                                debug!("create version reply packet");
+                                debug!("transfer version reply packet");
                             }
                         }
                     }
@@ -72,8 +76,31 @@ impl Collaboration {
         
     }
 
-    fn handle_ping(&self, _sender: &PublicKey, _bytes: Option<&[u8]>) {
-        
+    fn handle_ping(&self, sender: &PublicKey, _bytes: Option<&[u8]>) {
+        // send pong:
+        let mut output: Vec<u8> = Vec::<u8>::new();
+        match pack_pong(&mut output, self.sequence, self.round) {
+            Err(_) => {
+                error!("failed to serialize pong");
+            },
+            Ok(_) => {
+                match Packet::new(*sender, output) {
+                    None => {
+                        error!("failed create pong packet");
+                    },
+                    Some(pack) => {
+                        match self.tx_send.send(pack) {
+                            Err(e) => {
+                                warn!("failed send pong packet: {}", e);
+                            },
+                            Ok(_) => {
+                                debug!("transfer pong packet");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     fn handle_pong(&self, _sender: &PublicKey, _bytes: Option<&[u8]>) {
@@ -111,38 +138,48 @@ impl Collaboration {
     }
 }
 
-fn pack_version_reply(output: &mut Vec<u8>) -> bincode::Result<()> {
-    let cmd_len = 1 + 1 + 2 + 8 + 8 + 8;
+fn pack_version_request(output: &mut Vec<u8>) -> bincode::Result<()> {
+    let cmd_len = 1 + 1; // flags + cmd
     output.reserve(cmd_len);
-    
-    let flags = Flags::N;        
-    let cmd = Command::VersionReply as u8;
+    serialize_into(output.by_ref(), &Flags::N.bits())?;
+    serialize_into(output.by_ref(), &(Command::VersionReply as u8))?;
 
-    serialize_into(output.by_ref(), &flags.bits())?;
-    serialize_into(output.by_ref(), &cmd)?;
+    Ok(())
+}
+
+fn pack_version_reply(output: &mut Vec<u8>, sequence: u64, round: u64) -> bincode::Result<()> {
+    let cmd_len = 1 + 1 + 2 + 8 + 8 + 8; // flags + cmd + version + uuid + sequence + round
+    output.reserve(cmd_len);
+
+    serialize_into(output.by_ref(), &Flags::N.bits())?;
+    serialize_into(output.by_ref(), &(Command::VersionReply as u8))?;
     serialize_into(output.by_ref(), &NODE_VERSION)?;
     serialize_into(output.by_ref(), &UUID_TESTNET)?;
-    
-    let last_seq: u64 = 0;
-    serialize_into(output.by_ref(), &last_seq)?;
-    
-    let cur_round: u64 = 0; 
-    serialize_into(output.by_ref(), &cur_round)?;
+    serialize_into(output.by_ref(), &sequence)?;
+    serialize_into(output.by_ref(), &round)?;
 
     assert_eq!(cmd_len, output.len());
 
     Ok(())
 }
 
-fn pack_version_request(output: &mut Vec<u8>) -> bincode::Result<()> {
-    let cmd_len = 1 + 1 + 2 + 8 + 8 + 8;
+fn pack_ping(output: &mut Vec<u8>) -> bincode::Result<()> {
+    let cmd_len = 1 + 1; // flags + cmd
     output.reserve(cmd_len);
-    
-    let flags = Flags::N;        
-    let cmd = Command::VersionReply as u8;
-
-    serialize_into(output.by_ref(), &flags.bits())?;
-    serialize_into(output.by_ref(), &cmd)?;
+    serialize_into(output.by_ref(), &Flags::N.bits())?;
+    serialize_into(output.by_ref(), &(Command::Ping as u8))?;
 
     Ok(())
+}
+
+fn pack_pong(output: &mut Vec<u8>, sequence: u64, round: u64) -> bincode::Result<()> {
+    let cmd_len = 1 + 1 + 8 + 8; // flags + cmd + sequence + round
+    output.reserve(cmd_len);
+
+    serialize_into(output.by_ref(), &Flags::N.bits())?;
+    serialize_into(output.by_ref(), &(Command::Pong as u8))?;
+    serialize_into(output.by_ref(), &sequence)?;
+    serialize_into(output.by_ref(), &round)?;
+
+    Ok(())    
 }
