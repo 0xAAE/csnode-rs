d7ed866471bd172730b7ad6a249ab2c4209ebcb7
aav@credits.com <aav@credits.com>
2020-05-04 13:24:06 +0300

    Rename and rearrange modules as in R-2018

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -5,10 +5,9 @@ pub mod caches;
 pub mod raw_block;
 mod raw_block_tests;
 
-//use ruspiro_singleton::Singleton;
-
 pub type SharedBlocks = Arc<RwLock<blocks::Blocks>>;
 pub type SharedCaches = Arc<RwLock<caches::Caches>>;
 
+//use ruspiro_singleton::Singleton;
 // pub static BLOCKS: Singleton<blocks::Blocks> = Singleton::new(blocks::Blocks::new());
 // pub static CACHES: Singleton<caches::Caches> = Singleton::new(caches::Caches::new());
